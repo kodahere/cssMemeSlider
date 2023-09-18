72144a9c3c2534299decf9b6559149d29abb907f
Vlad <repovlad@gmail.com>
2023-09-18 03:17:17 +0300

    feat: add js content for active nav button

---
+++ cssMemeSlider/index.js
@@ -0,0 +1,10 @@
+let header = document.getElementById("myDIV")
+let btns = header.getElementsByClassName("btn")
+
+for(let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    })
+}
